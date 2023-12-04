7a155b29311bce022a1426de5707ee2c6bb308be
willgregoire <139147076+willgregoire@users.noreply.github.com>
2023-12-04 16:14:32 -0500

    cleanup for last commit

--- src/module/llm-text-content-importer.ts
+++ src/module/llm-text-content-importer.ts
@@ -6,35 +6,6 @@ Hooks.on('ready', async () => {
   console.log('llm-text-content-importer Ready!!');
 });
 
-// Show the
-// Hooks.on('renderActorSheet', (actorSheet, html, actor) => {
-//   log('rendering actor sheet: ', actorSheet, html, actor);
-
-//   const button = $('<button type="button" id="llmFlavorerButton"><div>Flavorer</div></button>');
-
-//   button.click((event) => {
-//     log('clicked flavorer button!');
-//     new FlavorerForm(FlavorerForm.defaultOptions, actor.actor).render(true);
-//   });
-//   const titleElement = html.closest('.app').find('.charname');
-//   if (!actorSheet._minimized) button.insertAfter(titleElement);
-//   log('inserted button');
-// });
-
-/*
-
-  $('.directory-header .header-actions', $('[data-tab="actors"]'))
-    .filter((i, e) => !$(e).has('#hct-directory-button').length)
-    .append(
-      `<button id='hct-directory-button' data-hct_start><i class='fas fa-dice-d20'></i>${game.i18n.localize(
-        'HCT.ActorsDirectoryButton',
-      )}</button>`,
-    );
-  $('[data-hct_start]').on('click', function () {
-    if (userHasRightPermissions()) app.openForNewActor();
-  });
-  */
-
 Hooks.on('renderActorDirectory', () => {
   $('.directory-header .header-actions', $('[data-tab="actors"]'))
     .filter((i, e) => !$(e).has('#llm-text-content-importer-button').length)
