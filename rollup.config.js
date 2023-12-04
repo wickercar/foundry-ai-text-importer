d8ce4bfbc8a35f69b3435d816b331a0803cf366f
willgregoire <139147076+willgregoire@users.noreply.github.com>
2023-12-04 15:15:05 -0500

    initial commit - new module based on llm-controlled-turn

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+const typescript = require('rollup-plugin-typescript2');
+const { nodeResolve } = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+
+module.exports = {
+  input: 'src/module/llm-text-content-importer.ts',
+  output: {
+    dir: 'dist/module',
+    format: 'es',
+    sourcemap: true,
+  },
+  plugins: [
+    nodeResolve(),
+    typescript({}),
+    commonjs({
+      include: /node_modules/,
+      requireReturnsDefault: 'auto', // <---- this solves default issue
+    }),
+  ],
+};
---
+++ src/module/llm-text-content-importer.ts
@@ -0,0 +1,7 @@
+Hooks.once('init', async () => {
+  console.log('llm-text-content-importer Initialized!!');
+});
+
+Hooks.on('ready', async () => {
+  console.log('llm-text-content-importer Ready!!');
+});
