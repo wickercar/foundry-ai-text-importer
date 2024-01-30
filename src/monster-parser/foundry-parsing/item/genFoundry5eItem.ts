315dfe06a5f93fa1c5ae6695952c29cbcf846919
willgregoire <139147076+willgregoire@users.noreply.github.com>
2024-01-30 17:19:01 -0500

    rename foundry item parsing files to include 5e (could merge with previous)

---
+++ src/monster-parser/foundry-parsing/item/genFoundry5eItem.ts
@@ -0,0 +1,28 @@
+import OpenAILLM from '../../openaillm';
+import { searchMonsterDBForItemWithName } from '../searchFoundryMonsterDB';
+import { Foundry5eItem, Foundry5eItemSchema } from '../../schemas/foundry/item/Foundry5eItem';
+import { PromptTemplate } from 'langchain/prompts';
+import { StructuredOutputParser } from 'langchain/output_parsers';
+import { LLMChain } from 'langchain/chains';
+import { z } from 'zod';
+import { genCustomFoundryItem } from './genCustomFoundryItem';
+import { genFoundryItemFromExample } from './genFoundryItemFromExample';
+
+// this is very rudimentary for now, just generating a blank item with the same name and description, using an existing item as a template if found
+export const genFoundryItemFromNameAndText = async (warfItemNameAndText: {
+  name: string;
+  text: string;
+}): Promise<Foundry5eItem> => {
+  const { name, text } = warfItemNameAndText;
+  // TODO - investigate porting over WarfItemToFoundryConverter from server code
+
+  const itemWithSameName = await searchMonsterDBForItemWithName(warfItemNameAndText.name);
+  // Option 1 - try to find an example and use it as inspiration
+  if (itemWithSameName != null) {
+    return genFoundryItemFromExample(itemWithSameName, name, text);
+  } else {
+    // TODO - super rudimentary for now, just generating a blank item with the same name and description
+    return genCustomFoundryItem(name, text);
+  }
+  // Option 2 - gen full new item from scratch
+};
