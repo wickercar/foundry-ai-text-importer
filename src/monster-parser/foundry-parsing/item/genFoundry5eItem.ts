d56269d8314ccb285ee881448a6efb6b70e242b9
willgregoire <139147076+willgregoire@users.noreply.github.com>
2024-01-30 18:30:27 -0500

    (squash) small naming updates

--- src/monster-parser/foundry-parsing/item/genFoundry5eItem.ts
+++ src/monster-parser/foundry-parsing/item/genFoundry5eItem.ts
@@ -5,14 +5,14 @@ import { genCustomFoundryItem } from './genCustomFoundry5eItem';
 import { genFoundryItemFromExample } from './genFoundry5eItemFromExample';
 
 // this is very rudimentary for now, just generating a blank item with the same name and description, using an existing item as a template if found
-export const genFoundryItemFromNameAndText = async (warfItemNameAndText: {
+export const genFoundryItemFromNameAndText = async (itemNameAndText: {
   name: string;
   text: string;
 }): Promise<Foundry5eItem> => {
-  const { name, text } = warfItemNameAndText;
+  const { name, text } = itemNameAndText;
   // TODO - investigate porting over WarfItemToFoundryConverter from server code
 
-  const itemWithSameName = await searchMonsterDBForItemWithName(warfItemNameAndText.name);
+  const itemWithSameName = await searchMonsterDBForItemWithName(name);
   // Option 1 - try to find an example and use it as inspiration
   if (itemWithSameName != null) {
     return genFoundryItemFromExample(itemWithSameName, name, text);
