3a9a132077355a679ed0730ef7440f11770d0310
willgregoire <139147076+willgregoire@users.noreply.github.com>
2024-01-30 16:52:37 -0500

    Foundry Compendium search (by exact name match only)
    - Functionality to search through an existing foundry db for an example item that has the exact same name in order to use as an example
    - Works off of a monsters.db file included in this file, should eventually be able to search the user's compendium

---
+++ src/monster-parser/foundry-parsing/searchFoundryMonsterDB.ts
@@ -0,0 +1,44 @@
+import fs from 'fs';
+import path from 'path';
+import { Foundry5eItem } from '../schemas/foundry/item/Foundry5eItem';
+
+export const searchMonsterDBForItemWithName = async (name: string): Promise<Foundry5eItem | null> => {
+  return new Promise((resolve, reject) => {
+    // Path to the monsters.db file
+    const filePath = path.join(__dirname, 'src/monster-parser/srd/monsters.db');
+
+    // Read the file
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      // Split the file content by newlines
+      const lines = data.split('\n');
+
+      // Search each JSON object
+      for (const line of lines) {
+        try {
+          const monster = JSON.parse(line);
+          // iterate through the items
+          for (const item of monster.items) {
+            if (item.name === name) {
+              console.log('matching item found!');
+              resolve(item as Foundry5eItem);
+              return;
+            }
+          }
+        } catch (error) {
+          // Error in parsing a line (might not be a valid JSON)
+          continue;
+        }
+      }
+
+      // If the monster is not found
+      resolve(null);
+    });
+  });
+};
+
+// assumes monsters.db is a bunch of json objects separated by newlines
