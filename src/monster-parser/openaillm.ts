814f194eabc2b3ab67d147a7385c9fe8a8e7fd17
willgregoire <139147076+willgregoire@users.noreply.github.com>
2024-01-31 02:28:36 -0500

    read openAIKey from localStorage (remove placeholder)

--- src/monster-parser/openaillm.ts
+++ src/monster-parser/openaillm.ts
@@ -1,10 +1,11 @@
 import { OpenAI } from 'langchain/llms/openai';
+import OpenAIAPIKeyStorage from './settings/openai-api-key/OpenAIAPIKeyStorage';
 
 const OpenAILLM = () =>
   new OpenAI({
     modelName: 'gpt-4',
     temperature: 0,
-    openAIApiKey: 'placeholder-key',
+    openAIApiKey: OpenAIAPIKeyStorage.getApiKey(),
   });
 
 export default OpenAILLM;
