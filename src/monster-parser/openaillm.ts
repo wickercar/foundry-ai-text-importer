36a47330e696095b7cd8eac50dceb1c633526de6
willgregoire <139147076+willgregoire@users.noreply.github.com>
2024-01-31 22:37:22 -0500

    use gpt-4-turbo-preview
    - this is showing slightly faster speeds, going to update to this for now
    - probably going to add a model selector

--- src/monster-parser/openaillm.ts
+++ src/monster-parser/openaillm.ts
@@ -3,7 +3,7 @@ import OpenAIAPIKeyStorage from './settings/openai-api-key/OpenAIAPIKeyStorage';
 
 const OpenAILLM = () =>
   new OpenAI({
-    modelName: 'gpt-4',
+    modelName: 'gpt-4-turbo-preview',
     temperature: 0,
     openAIApiKey: OpenAIAPIKeyStorage.getApiKey(),
   });
