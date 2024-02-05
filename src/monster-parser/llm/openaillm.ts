6c73bc96fd8186be89dda824a15108b5e0d2df10
willgregoire <139147076+willgregoire@users.noreply.github.com>
2024-02-05 13:16:44 -0500

    add log for gpt model

--- src/monster-parser/llm/openaillm.ts
+++ src/monster-parser/llm/openaillm.ts
@@ -2,7 +2,8 @@ import { OpenAI } from 'langchain/llms/openai';
 import OpenAIAPIKeyStorage from '../settings/openai-api-key/OpenAIAPIKeyStorage';
 
 const OpenAILLM = () => {
-  const modelSetting = game.settings.get('llm-text-content-importer', 'openaiModel');
+  const modelSetting = game.settings.get('llm-text-content-importer', 'openaiModel') as string;
+  console.log('initializing OpenAI with model: ', modelSetting);
   return new OpenAI({
     modelName: game.settings.get('llm-text-content-importer', 'openaiModel') as string,
     temperature: 0,
